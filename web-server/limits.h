ca872ed6addd6cdef9423049a6e2ef0e76dfa616
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-04-12 18:39:29 +0200

    working on it

--- web-server/limits.h
+++ web-server/limits.h
@@ -2,8 +2,8 @@
 #define __LIMITS_H
 
 /* Some upper bound values */
-#define METHOD_SIZE 8
-#define PATH_SIZE 128
+#define MAX_METHOD_SIZE 8
+#define MAX_PATH_SIZE 128
 #define MAX_NUM_HDR 16
 
 #endif
--- web-server/web_server_accel.c
+++ web-server/web_server_accel.c
@@ -1,4 +1,3 @@
-#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -9,28 +8,78 @@
 #include "libs/picohttpparser.h"
 #include "limits.h"
 
+#ifndef __ANNOTATE_LOOP
+#define __ANNOTATE_LOOP(x)
+#endif
+
+#ifndef __u32
+typedef unsigned long long int __u64;
+typedef unsigned int           __u32;
+typedef unsigned short         __u16;
+typedef unsigned char          __u8 ;
+#endif
+
 struct parsed_data {
 	char *method;
-	size_t method_len;
+	__u16 method_len;
 	char *path;
-	size_t path_len;
+	__u16 path_len;
 	int minor_version;
 	struct phr_header headers[MAX_NUM_HDR];
-	size_t num_headers;
+	__u16 num_headers;
 };
 
-/* Entry function */
-void loop(void)
-{
-	int ret = 0;
-	int fd = 0;
-	char *buf = NULL;
-	size_t size = 0;
+struct per_conn_state {
+	__u8 is_old;
+	__u16 parsed_off;
 	struct parsed_data preq;
-	memset(&preq, 0, sizeof(preq));
+};
+
+/*
+ * Check if server expects a new request on the connection. If so, initialize
+ * the state.
+ * */
+void _check_new_req(struct per_conn_state *state)
+{
+	if (!state->is_old) {
+		state->is_old = 1;
+		state->parsed_off = 0;
+		memset(&state->preq, 0, sizeof(struct parsed_data));
+	}
+}
+
+/* Entry function */
+void loop(struct per_conn_state state)
+{
+	int ret;
+	int fd;
+	char *buf;
+	__u16 size;
+	struct parsed_data *preq;
+
+	_check_new_req(&state);
+	preq = &state.preq;
 
 	size = read(fd, buf, 0);
-
-	printk("hello");
-	/* ret = phr_parse_request(buf, size, ) */
+	ret = phr_parse_request(buf, size, &preq->method, &preq->method_len,
+			&preq->path, &preq->path_len, &preq->minor_version,
+			preq->headers, &preq->num_headers, state.parsed_off);
+	if (ret < 0) {
+		if (ret == -1) {
+			/* Failed to parse the request */
+			return;
+		} else if (ret == -2) {
+			/* Partial data */
+			state.parsed_off += size;
+			return;
+		}
+	}
+	/* Finish parsing the HTTP/1.1 */
+	state.parsed_off = 0;
+	char path[128];
+	memset(path, 0, 128);
+	__ANNOTATE_LOOP(127);
+	strncpy(path, preq->path, preq->path_len);
+	path[preq->path_len] = 0;
+	printk("path: %s", path);
 }
